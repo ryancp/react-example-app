54d988612bf6a5879282478b60e623cab97f6d67
Ryan <ryan.pharis@gmail.com>
2019-03-07 20:43:10 -0600

    Commit built version for gh pages

---
+++ precache-manifest.27b27fb82e6a30252e680d003fbb980f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d013cdee371d222345b",
+    "url": "/static/css/main.10465997.chunk.css"
+  },
+  {
+    "revision": "4d013cdee371d222345b",
+    "url": "/static/js/main.98ed901f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ebf67ce8c6844fe763ff",
+    "url": "/static/js/2.0966d7c2.chunk.js"
+  },
+  {
+    "revision": "aa98397173f4433593f9f7b359305f2e",
+    "url": "/index.html"
+  }
+];
--- src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import teams from "../../containers/teams/reducer.js";
-
-const rootReducer = combineReducers({
-  teams,
-});
-
-export default rootReducer;
